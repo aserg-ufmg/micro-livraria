195696c8c5faaa7909b5a29011a402042a2d569d
Rodrigo Brito <rig.10@hotmail.com>
2021-02-09 16:39:07 -0300

    feat(shipping): include shipping request

--- services/frontend/index.js
+++ services/frontend/index.js
@@ -13,40 +13,78 @@ function newBook(book) {
                 </figure>
             </div>
             <div class="card-content">
-                <div class="content">
-                    <p class="is-size-4">R$${book.price.toFixed(2)}</p>
-                    <p class="is-size-6">Disponível em estoque: 5</p>
-                    <h4 class="is-size-3 title">${book.name}</h4>
-                    <p class="subtitle">${book.author}</p>
+                <div class="content book" data-id="${book.id}">
+                    <div class="book-meta">
+                        <p class="is-size-4">R$${book.price.toFixed(2)}</p>
+                        <p class="is-size-6">Disponível em estoque: 5</p>
+                        <h4 class="is-size-3 title">${book.name}</h4>
+                        <p class="subtitle">${book.author}</p>
+                    </div>
                     <div class="field has-addons">
                         <div class="control">
                             <input class="input" type="text" placeholder="Digite o CEP" />
                         </div>
                         <div class="control">
-                            <a class="button is-info"> Calcular Frete </a>
+                            <a class="button button-shipping is-info" data-id="${book.id}"> Calcular Frete </a>
                         </div>
                     </div>
-                    <button class="button is-success is-fullwidth">Comprar</button>
+                    <button class="button button-buy is-success is-fullwidth">Comprar</button>
                 </div>
             </div>
         </div>`;
     return div;
 }
 
-const books = document.querySelector('.books');
+function calculateShipping(id, cep) {
+    fetch('http://localhost:3000/shipping/' + cep)
+        .then((data) => {
+            if (data.ok) {
+                return data.json();
+            }
+            throw data.statusText;
+        })
+        .then((data) => {
+            swal('Frete', `O frete é: R$${data.value.toFixed(2)}`, 'success');
+        })
+        .catch((err) => {
+            swal('Erro', 'Erro ao consultar frete', 'error');
+            console.error(err);
+        });
+}
 
 document.addEventListener('DOMContentLoaded', function () {
+    const books = document.querySelector('.books');
+
     fetch('http://localhost:3000/products')
         .then((data) => {
             if (data.ok) {
                 return data.json();
             }
+            throw data.statusText;
         })
         .then((data) => {
             if (data) {
                 data.forEach((book) => {
                     books.appendChild(newBook(book));
                 });
+
+                document.querySelectorAll('.button-shipping').forEach((btn) => {
+                    btn.addEventListener('click', (e) => {
+                        const id = e.target.getAttribute('data-id');
+                        const cep = document.querySelector(`.book[data-id="${id}"] input`).value;
+                        calculateShipping(id, cep);
+                    });
+                });
+
+                document.querySelectorAll('.button-buy').forEach((btn) => {
+                    btn.addEventListener('click', (e) => {
+                        swal('Compra de livro', 'Sua compra foi realizada com sucesso', 'success');
+                    });
+                });
             }
+        })
+        .catch((err) => {
+            swal('Erro', 'Erro ao listar os produtos', 'error');
+            console.error(err);
         });
 });
